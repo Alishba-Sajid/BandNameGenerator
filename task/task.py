b62539dbb17ec2253c54b6496f02fee1cc763d36
Alishba-Sajid <alishbasajid121@gmail.com>
2024-11-22 18:51:15 +0800

    Initial commit

---
+++ task/task.py
@@ -0,0 +1,4 @@
+print("Welcome to  the Band Name Generaotor")
+city = input("What is the name of the city you grew up in? \n")
+pet_name = input("What is the name of your pet? \n")
+print("The name of your band could be " + city + " " + pet_name)
